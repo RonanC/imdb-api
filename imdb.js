6454cd39740c6c479c1dbf1731f3c8f0067a9ffd
Ronan Connolly <hi@ronanconnolly.ie>
2015-10-11 20:39:59 +0100

    add new routes

--- imdb.js
+++ imdb.js
@@ -1,5 +1,6 @@
 // Import the fs module so that we can read in files.
 var fs = require('fs');
+
 // Import express to create and configure the HTTP server.
 var express = require('express');
 
@@ -16,8 +17,62 @@ app.get('/', function(req, res) {
 
 // Send back the JSON for movie i at /movie/id/i.
 app.get('/movie/id/:id', function(req, res) {
-  res.json(data[req.params.id]);
+  res.json(data[req.params.id].title);
+});
+
+// Send back the JSON for movie i at /movie/year/i
+app.get('/movie/year/:year', function(req, res){
+  var result = [];
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].year+"" === req.params.year+"") {
+      result.push(data[i].title);
+    }
+  }
+
+  res.json(result);
+});
+
+// random movie
+app.get('/movie/random', function(req, res){
+  var max = data.length;
+  var min = 1;
+  var rand;
+
+  var nomovies = req.query.nomovies;
+  if (nomovies > 0) {
+    var result = [];
+
+    for (var i = 0; i < nomovies; i++) {
+      rand = Math.floor(Math.random() * (max - min + 1)) + min;
+      console.log(rand);
+      result.push(data[rand].title);
+    }
+    res.json(result);
+  }
+  else {
+    rand = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(rand);
+    res.json(data[rand].title);
+  }
+
+});
+
+// search route with part of title name
+app.get('/movie/search/:search', function(req, res){
+  var result = [];
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].title.indexOf(req.params.search) >= 0) {
+      console.log(data[i].title);
+      result.push(data[i].title);
+    }
+  }
+
+  res.json(result);
 });
 
 // Start the server.
 var server = app.listen(8000);
+
+console.log("Web Service running on local host port 8000");
